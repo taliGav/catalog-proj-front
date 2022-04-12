20ab466bf726c75aa593f82de2a9d5393aef4216
Ben <Benl000100@gmail.com>
2022-04-12 15:04:03 +0300

    scss catalog

--- src/views/catalog-app.vue
+++ src/views/catalog-app.vue
@@ -1,34 +1,34 @@
 <template>
-  <section class="catalog-app">
+  <section class="catalog-app page-layout">
     <toy-filter @setFilter="setFilter" />
     <toy-list :toys="toys" />
   </section>
 </template>
 
 <script>
-import toyList from '../components/toy-list.vue'
-import toyFilter from '../components/toy-filter.vue'
+import toyList from "../components/toy-list.vue";
+import toyFilter from "../components/toy-filter.vue";
 export default {
-  name: 'catalog-app',
+  name: "catalog-app",
   created() {
-    this.$store.dispatch({ type: 'loadProducts' })
+    this.$store.dispatch({ type: "loadProducts" });
   },
   computed: {
     toys() {
-      return this.$store.getters.toysToShow
+      return this.$store.getters.toysToShow;
     },
   },
   methods: {
     setFilter(filterBy) {
-      const copyfilter = JSON.parse(JSON.stringify(filterBy))
-      this.$store.dispatch({ type: 'setFilter', filterBy: copyfilter })
+      const copyfilter = JSON.parse(JSON.stringify(filterBy));
+      this.$store.dispatch({ type: "setFilter", filterBy: copyfilter });
     },
   },
   components: {
     toyList,
     toyFilter,
   },
-}
+};
 </script>
 
 <style></style>
