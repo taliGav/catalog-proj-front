948209b85ec74cf6c28f3e02fc7b35e1ac982e92
Ben <Benl000100@gmail.com>
2022-04-12 15:59:22 +0300

    scss filter

--- src/components/product-filter.vue
+++ src/components/product-filter.vue
@@ -1,15 +1,25 @@
 <template>
   <section class="product-filter">
-    <button @click="isOpen = !isOpen">{{ isOpen ? 'Close' : 'Open' }} options</button>
-    <form @submit.prevent="" v-if="isOpen" class="form items-center gap-1 py-1">
+    <form @submit.prevent="" v-if="isOpen">
       <div class="form-control">
-        <label for="name" class="form-label">Name</label>
-        <el-input @input="setFilter" v-model="filterBy.name" id="name" class="w-50" size="large" placeholder="Search by product name..." />
+        <el-input
+          @input="setFilter"
+          v-model="filterBy.name"
+          id="name"
+          class="w-50"
+          size="large"
+          placeholder="Search"
+        />
       </div>
 
       <div class="form-control">
-        <label for="stock" class="form-label">In Stock</label>
-        <el-select @change="setFilter" v-model="filterBy.inStock" id="stock" size="large">
+        <el-select
+          @change="setFilter"
+          v-model="filterBy.inStock"
+          id="stock"
+          size="large"
+          placeholder="Age"
+        >
           <el-option label="All" value="" />
           <el-option label="In Stock" value="true" />
           <el-option label="Not in stock" value="false" />
@@ -17,15 +27,33 @@
       </div>
 
       <div class="form-control">
-        <label for="labels" class="form-label">By labels</label>
-        <el-select multiple @change="setFilter" v-model="filterBy.labels" collapse-tags collapse-tags-tooltip id="labels" size="large">
-          <el-option v-for="label in labels" :key="label" :label="label" :value="label" />
+        <el-select
+          placeholder="Budget"
+          multiple
+          @change="setFilter"
+          v-model="filterBy.labels"
+          collapse-tags
+          collapse-tags-tooltip
+          id="labels"
+          size="large"
+        >
+          <el-option
+            v-for="label in labels"
+            :key="label"
+            :label="label"
+            :value="label"
+          />
         </el-select>
       </div>
 
       <div class="form-control">
-        <label for="sort" class="form-label">Sort by</label>
-        <el-select @change="setFilter" v-model="filterBy.sortBy" id="sort" size="large">
+        <el-select
+          placeholder="Age"
+          @change="setFilter"
+          v-model="filterBy.sortBy"
+          id="sort"
+          size="large"
+        >
           <el-option label="Time" value="time" />
           <el-option label="Name" value="name" />
           <el-option label="Price" value="price" />
@@ -36,33 +64,33 @@
 </template>
 
 <script>
-import { utilService } from '../services/util-service'
+import { utilService } from "../services/util-service";
 
 export default {
-  name: 'product-filter',
+  name: "product-filter",
   data() {
     return {
       filterBy: {
-        name: '',
+        name: "",
         labels: [],
-        inStock: '',
-        sortBy: '',
+        inStock: "",
+        sortBy: "",
       },
       isOpen: true,
-    }
+    };
   },
   created() {
-    this.setFilter = utilService.debounce(this.setFilter, 500)
+    this.setFilter = utilService.debounce(this.setFilter, 500);
   },
   computed: {
     labels() {
-      return this.$store.getters.labels
+      return this.$store.getters.labels;
     },
   },
   methods: {
     setFilter() {
-      this.$emit('set-filter', { ...this.filterBy })
+      this.$emit("set-filter", { ...this.filterBy });
     },
   },
-}
+};
 </script>
